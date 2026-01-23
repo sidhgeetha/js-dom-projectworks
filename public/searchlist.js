cad602dc15e62d765f2242527a2407827b8a0fd4
Geetha Sidhambaram <sidh.geetha@gmail.com>
2026-01-23 18:25:02 +0000

    searc/filter task

--- public/searchlist.js
+++ public/searchlist.js
@@ -1,5 +1,20 @@
-  const inputEl = document.getElementById("search");
+const inputEl = document.getElementById("search"); //finding search box
+const items = document.querySelectorAll(".list"); //see all lists items with name class name given "list"
 
-  const searchItem = inputEl.value;
+inputEl.addEventListener("input", function () {   //add even listener wehn type itesm to search , input 
+  const value = inputEl.value.toLowerCase();           //chnage all to lovercase
 
-  const programList = document.querySelectorAll(".list");
+  for (let i = 0; i < items.length; i++) {            //loop each item using loop
+    const text = items[i].textContent.toLowerCase();   //read each items itesm[i], itesm[0] = first item
+
+    if (text.includes(value)) {
+      //if searched word is matching .inclued in list, than show only that item
+      items[i].style.display = "block";
+    } else {
+      items[i].style.display = "none";
+      const showError = document.getElementById("error");
+      showError.innerText=`no words found`;
+      showError.classList.add("text-red-400" , "text-sm");
+    }
+  }
+});
